acc72ac03515adf7fd76475e41179c7dc314695d
Craig A. Cook <coyot3@berkeley.edu>
2014-10-09 11:47:29 -0700

    CYB-143, CYB-168, and CYB-170.  If students are not present in the DataCultures database, add them.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,24 +1,30 @@
 class Student < ActiveRecord::Base
   has_many :activities
 
-  @@students_by_id = nil
+  def self.ensure_student_record_exists_by_canvas_id(canvas_id)
+    if Student.find_by_canvas_user_id(canvas_id).nil?
+      request_object = Canvas::ApiRequest.new({base_url: AppConfig::CourseConstants.base_url, api_key: AppConfig::CourseConstants.api_key})
+      profile_response = request_object.request.get('api/v1/users/'+canvas_id.to_s+'/profile')
+      profile = profile_response.body
+      student_data = {share: false, has_answered_share_question: false, canvas_user_id: canvas_id,
+                      sis_user_id: -1, section: 'Unknown',
+                      primary_email: profile['primary_email'],
+                      name: profile['name'], sortable_name: profile['sortable_name'] }
+      Student.create(student_data)
+    end
 
-  def self.aggregateFromClass(course_id)
-    # get a list of students using an api call
-    # GET /api/v1/courses/:course_id/users
-
-    # fill up their data in the database
-    # GET /api/v1/users/:user_id/profile
   end
 
+
+  # expire cache of students in the current course
   def self.reset_students_by_id!
-    @@students_by_id = nil
   end
 
+  # This data is very cachable
   def self.get_students_by_id
     ## using "pluck" gives the less readable [0] and [1] compared to ".id" and ".name" but it does not
     #    instantiate an ActiveRecord object for each.
-    @@students_by_id = @@students_by_id || Student.all.pluck(:id, :name).inject({}){|memo, student|
+    Student.all.pluck(:id, :name).inject({}){|memo, student|
        memo.merge!({student[0] => student[1]})}
   end
 
