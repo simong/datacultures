8dfac97f4a2802b8a60f0387edb442b50dd1b674
Craig A. Cook <coyot3@berkeley.edu>
2014-10-30 16:29:37 -0700

    CYB-230  Refactor the Comments API
    * code is far safer (handles bad input better)
    * now scores when a comment is posted (subject to comment scoring being turned on -- still creates
       the entry but marks it as not scoring, as per other scoring cases)
    * Far better spec coverage

--- spec/controllers/api/v1/comments_controller_spec.rb
+++ spec/controllers/api/v1/comments_controller_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CommentsController, :type => :controller do
 
-  let(:valid_comment_json) {
+  let(:valid_create_comment_json) {
     {
-      id: 10,
+      id: '10',
       comment: "This is a practice comment"
+    }
+  }
+
+  let(:valid_update_comment_json) {
+    {
+        comment: 'Some comment value',
+        comment_id: 100
     }
   }
 
@@ -14,42 +21,62 @@ RSpec.describe Api::V1::CommentsController, :type => :controller do
     Activity.delete_all
     PointsConfiguration.create({interaction: "GalleryComment", points_associated: 7,
                                 active: true, pcid: 3})
+    FactoryGirl.create(:student, name: 'Ford Prefect of the Comments Controller', canvas_user_id: 5)
   end
 
-  # describe "POST update" do
-  #
-  #   before(:all) do
-  #     FactoryGirl.create(:student, name: 'Ford Prefect of the Comments Controller', canvas_user_id: 5)
-  #   end
-  #
-  #   after(:all) do
-  #     Student.delete_all
-  #   end
-  #
-  #   before(:each) do
-  #     #allow(controller).to receive(:current_user).and_return(USER_STRUCT)
-  #   end
-  #
-  #   it "creates a comment when submitted to with valid params" do
-  #    # allow(controller).to receive(:current_user).and_return(USER_STRUCT)
-  #     expect{
-  #       post :create, valid_comment_json
-  #     }.to change(Comment, :count).by(1)
-  #   end
-  #
-  #   it "responds with a 400 with invalid params" do
-  #     valid_comment_json.delete(:attachment_id)
-  #     post :create, valid_comment_json
-  #     expect(response.response_code).to eq(400)
-  #   end
-  #
-  #   it "does not create a new object with invalid params" do
-  #     expect{
-  #       valid_comment_json.delete(:attachment_id)
-  #       post :create, valid_comment_json
-  #     }.to change(Comment, :count).by(0)
-  #   end
-  #
-  # end
+  after(:all) do
+    Student.delete_all
+    Activity.delete_all
+    Comment.delete_all
+    PointsConfiguration.delete_all
+  end
+
+  before(:each) do
+    allow(controller).to receive(:current_user).and_return(USER_STRUCT)
+  end
+
+  describe "POST create" do
+
+    it "creates a comment when submitted to with valid params" do
+      expect{
+        post :create, valid_create_comment_json
+      }.to change(Comment, :count).by(1)
+    end
+
+    it "responds with a 400 with invalid params" do
+      valid_create_comment_json.delete(:id)
+      post :create, valid_create_comment_json
+      expect(response.response_code).to eq(400)
+    end
+
+    it "does not create a new object with invalid params" do
+      expect{
+        valid_create_comment_json.delete(:id)
+        post :create, valid_create_comment_json
+      }.to change(Comment, :count).by(0)
+    end
+
+    it "scores for the student posting" do
+      expect {
+        post :create, valid_create_comment_json
+      }.to change{Activity.student_scores}
+    end
+
+  end
+
+  describe "PUT update" do
+
+    it "returns '410 GONE' if not passed in a valid comment_id"  do
+      put :update, valid_update_comment_json
+      expect(response.response_code).to be(410)
+    end
+
+    it "updates the comment if given a valid comment_id" do
+      comment = Comment.create({gallery_id: 'FOO', authors_canvas_id: 5, content: 'Original Comment'})
+      put :update, valid_update_comment_json.merge({ comment_id: comment.id, comment: 'Changed Comment'})
+      expect(response.response_code).to be(200)
+    end
+
+  end
 
 end
