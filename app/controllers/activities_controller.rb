f4c13da5bc55b1c1d2aa361511629638c37c1f2b
Craig A. Cook <coyot3@berkeley.edu>
2014-12-15 11:14:38 -0800

    CYB-369 CSV must be required in production environment
    * production environment does not include CSV by default
    * dropping the draper gem means it was not being included, so
    * now it is explicitly included
    
    https://jira.ets.berkeley.edu/jira/browse/CYB-369

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,13 @@
 class ActivitiesController < ApplicationController
 
+  require 'csv'
+
   QUERY_SQL = <<-END_OF_QUERY_SQL
     SELECT
        act.reason AS "action", s.name AS "student name",
        (SELECT SUM(delta) FROM activities a2 WHERE ((a2.expired = 'f') AND (a2.assigned_discussion = 'f') AND (a2.score = 't')
           AND (a2.canvas_user_id=act.canvas_user_id))) AS "student total score",
-       act.delta AS "action score", act.updated_at AS "action at"
+       act.delta AS "action score", to_char(act.updated_at, 'YYYY-MM-DD HH24:MI:SS') AS "action at"
     FROM activities act INNER JOIN students s ON act.canvas_user_id = s.canvas_user_id
       WHERE (act.score = 't') AND (act.expired = 'f') AND (act.assigned_discussion = 'f')
       AND (act.reason IN ('Submission', 'Like', 'Dislike', 'DiscussionTopic', 'DiscussionEdit', 'DiscussionReply', 'Comment'));
