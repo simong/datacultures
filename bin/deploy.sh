4d1c1df1ccfeb221523a5115019aba4a5eaa1a0c
John Crossman <johncrossman@berkeley.edu>
2014-12-17 11:46:55 -0800

    CYB-303, Add Sidekiq restart to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -24,15 +24,25 @@ appHome="${HOME}/datacultures"
 cd ${appHome}
 
 git fetch origin \
-  || { echo 'FAILED to fetch latest from master' ; exit 1; }
+  || { echo 'FAILED to fetch latest from master'; exit 1; }
 
 change_count=$(git rev-list HEAD...origin/master | wc -l)
 
 if [ "$change_count" -gt 0 ];
   then
+    # Stop Sidekiq after capturing pid of process
+    pids_dir="${appHome}/tmp/pid"
+    sidekiq_pid="${pids_dir}/sidekiq.pid"
+    mkdir -p "${pids_dir}"
+    ps -f | grep sidekiq > "${sidekiq_pid}"
+    sidekiqctl stop "${sidekiq_pid}" \
+      || { echo 'FAILED to stop Sidekiq'; exit 1; }
+    ${appHome}/scripts/clear_sidekiq_queue.rb \
+      || { echo 'FAILED to clear Sidekiq queue'; exit 1; }
+
     # Create maintenace mode marker file
     touch ${DOCROOT}/datacultures-in-maintenance \
-      || { echo 'FAILED to put DataCultures into maintenance mode' ; exit 1; }
+      || { echo 'FAILED to put DataCultures into maintenance mode'; exit 1; }
 
     # discard any changes that now exist
     git reset --hard
@@ -42,7 +52,7 @@ if [ "$change_count" -gt 0 ];
     git pull origin master
 
     # get new gems if any
-    bundle --retry=5 || { echo 'FAILED to bundle app (e.g., getting new gems)' ; exit 1; }
+    bundle --retry=5 || { echo 'FAILED bundle DataCultures'; exit 1; }
 
     #  delete old assets
     rake assets:clobber
@@ -61,13 +71,18 @@ if [ "$change_count" -gt 0 ];
     thor keys:app_new_secret
 
     chmod 600 ${appHome}/config/secrets.yml \
-      || { echo 'FAILED to change permissions on yml file' ; exit 1; }
+      || { echo 'FAILED to change permissions on yml file'; exit 1; }
 
     # resume normal apache mode
     rm -f ${DOCROOT}/datacultures-in-maintenance \
-      || { echo 'FAILED to take DataCultures out of maintenance mode' ; exit 1; }
+      || { echo 'FAILED to take DataCultures out of maintenance mode'; exit 1; }
 
     # Touch file to restart Apache. See https://www.phusionpassenger.com/documentation/Users%20guide%20Apache.html
     touch ${appHome}/tmp/restart.txt
+
+    # Start Sidekiq
+    mkdir -p "${appHome}/tmp/log"
+    bundle exec sidekiq -L "${appHome}/tmp/log/sidekiq.log" -d \
+      || { echo 'FAILED to start Sidekiq'; exit 1; }
 
 fi
