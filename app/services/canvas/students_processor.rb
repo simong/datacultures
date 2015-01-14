aba547167243b1ac0ec76b4ada9ecf90f3263c6d
Nicolaas Matthijs <nicolaasmatthijs@googlemail.com>
2015-01-13 16:16:56 -0800

    CYB-409 - Engagement index: only 10 of 47 users appear on index initially
    CYB-409 - Fixed failing spec test

---
+++ app/services/canvas/students_processor.rb
@@ -0,0 +1,29 @@
+class Canvas::StudentsProcessor
+
+  attr_accessor :request_object
+
+  def initialize(conf)
+    @request_object = conf[:request_object] || ApiRequest.new(api_key: conf[:api_key], base_url: conf[:base_url])
+  end
+
+  def call(students)
+
+    students.each do |student|
+      student_attributes = {}
+      student_attributes[:name] = student["name"]
+      student_attributes[:sortable_name] = student["sortable_name"]
+      student_attributes[:canvas_user_id] = student["id"].to_i
+      student_attributes[:sis_user_id] = student["sis_user_id"] || -1
+      student_attributes[:section] = "Unknown"
+
+      # we can't use #find_or_create_by as if any other attributes differ but canvas_user_id value is already
+      # present, ActiveRecord will not match, and will try to create a new record, violating uniqueness on that key.
+      student_record = Student.where({canvas_user_id: student_attributes[:canvas_user_id]}).first
+      if student_record.nil?
+        Student.create(student_attributes)
+      else
+        student_record.update_attributes(student_attributes)
+      end
+    end
+  end
+end
--- config/initializers/retrieve_canvas_students.rb
+++ config/initializers/retrieve_canvas_students.rb
@@ -4,31 +4,9 @@ def retrieve_canvas_students()
   base_url = AppConfig::CourseConstants.base_url
   api_key = AppConfig::CourseConstants.api_key
   begin
-    response = ApiRequest.new(base_url:  base_url, api_key: api_key).request.get("api/v1/courses/#{course_id}/search_users")
-    users_list_json = response.body
-    status = response.status
-  rescue
-    users_list_json = {"errors" => true}
-  end
-  unless status != 200 || users_list_json.first.first == "errors" #If the Canvas API doesn't return a 200, data should not be read in
-    users_list_json.each do |user|
-      user_attributes = {}
-      user_attributes[:name] = user["name"]
-      user_attributes[:sortable_name] = user["sortable_name"]
-      user_attributes[:canvas_user_id] = user["id"].to_i
-      user_attributes[:sis_user_id] = user["sis_user_id"] || -1
-      user_attributes[:section] = "Unknown"
-
-      # we can't use #find_or_create_by as if any other attributes differ but canvas_user_id value is already
-      # present, ActiveRecord will not match, and will try to create a new record, violating uniqueness on that key.
-      student = Student.where({canvas_user_id: user_attributes[:canvas_user_id]}).first
-      if student.nil?
-        Student.create(user_attributes)
-      else
-        # pickup any changes from Canvas
-        student.update_attributes(user_attributes)
-      end
-    end
+    request_object = ApiRequest.new(base_url:  base_url, api_key: api_key)
+    Canvas::PagedApiProcessor.new({request_object: request_object,
+                                   handler: Canvas::StudentsProcessor.new({request_object: request_object})}).call("api/v1/courses/#{course_id}/search_users?per_page=250")
   end
 end
 
@@ -39,6 +17,3 @@ end
 if !Rails.env.test? && have_needed_config? && ActiveRecord::Base.connection.table_exists?('students')
   retrieve_canvas_students()
 end
-
-
-
