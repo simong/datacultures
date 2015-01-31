f32c99b3338c331803f90b839ae8c2a418cd0b12
Nicolaas Matthijs <nicolaasmatthijs@googlemail.com>
2015-01-30 18:05:32 -0800

    Renamed migration file back to original name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211233554) do
+ActiveRecord::Schema.define(version: 20150115105930105930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
