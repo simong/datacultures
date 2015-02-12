72e8ddc3b0a4351288e303a28cfb0e687d85f68b
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-02-12 11:57:00 -0800

    Use port 3100 during testing

--- spec/services/canvas/generic_url_processor_spec.rb
+++ spec/services/canvas/generic_url_processor_spec.rb
@@ -29,21 +29,21 @@ RSpec.describe Canvas::GenericUrlProcessor, type: :model do
     it 'creates the generic_url records' do
 
     # Stub the downloaded file
-    stub_request(:get, "https://localhost:3000/files/259732101/download?download_frd=1").
+    stub_request(:get, "https://localhost:3100/files/259732101/download?download_frd=1").
       with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
       to_return(:status => 200, :body => File.new('spec/fixtures/images/public-domain.jpg'), :headers => {})
 
     # Stub the "request-to-upload" request
-    stub_request(:post, "http://localhost:3000/api/v1/courses/1/files").
-      to_return(:status => 200, :body => '{"upload_url": "http://localhost:3000/files", "upload_params": {}}', :headers => {'Content-Type': 'application/json'})
+    stub_request(:post, "http://localhost:3100/api/v1/courses/1/files").
+      to_return(:status => 200, :body => '{"upload_url": "http://localhost:3100/files", "upload_params": {}}', :headers => {'Content-Type': 'application/json'})
     # Stub the actual upload
-    stub_request(:post, "http://localhost:3000/files").
-      to_return(:status => 302, :body => '', :headers => {'location' => 'http://localhost:3000/confirm'})
+    stub_request(:post, "http://localhost:3100/files").
+      to_return(:status => 302, :body => '', :headers => {'location' => 'http://localhost:3100/confirm'})
     # Stub the confirm request
-    stub_request(:post, "http://localhost:3000/confirm").
-      to_return(:status => 200, :body => '{"id": 1, "url": "http://localhost:3000/url/to/file"}', :headers => {'Content-Type': 'application/json'})
+    stub_request(:post, "http://localhost:3100/confirm").
+      to_return(:status => 200, :body => '{"id": 1, "url": "http://localhost:3100/url/to/file"}', :headers => {'Content-Type': 'application/json'})
     # Stub the hiding of the file
-    stub_request(:put, "http://localhost:3000/api/v1/files/1").
+    stub_request(:put, "http://localhost:3100/api/v1/files/1").
       with(:body => {"hidden"=>"true"}).
       to_return(:status => 200, :body => '', :headers => {})
 
