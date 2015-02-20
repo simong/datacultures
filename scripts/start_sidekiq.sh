bf94f2e8b0fd32c3b90a12cb65544d547444d00b
Nicolaas Matthijs <nicolaasmatthijs@googlemail.com>
2015-02-20 09:48:02 -0800

    CYB-457 - Duplicate thumbnails

--- scripts/start_sidekiq.sh
+++ scripts/start_sidekiq.sh
@@ -1,3 +1,3 @@
 mkdir -p tmp/log
 mkdir -p tmp/pids
-bundle exec sidekiq -d -c 10 -L tmp/log/sidekiq.log -P tmp/pids/sidekiq.pid
+bundle exec sidekiq -d -c 1s -L tmp/log/sidekiq.log -P tmp/pids/sidekiq.pid
