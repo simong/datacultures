dad6ce181cd6e8a63ed1c0e5c9c2c688eaef8c7f
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-04-20 18:35:29 +0100

    CYB-470: Running sum of activities in CSV export

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,11 +5,12 @@ class ActivitiesController < ApplicationController
   QUERY_SQL = <<-END_OF_QUERY_SQL
     SELECT
        act.reason AS "action", s.name AS "student name",
-       (SELECT SUM(delta) FROM activities a2 WHERE ((a2.expired = 'f') AND (a2.assigned_discussion = 'f') AND (a2.score = 't')
-          AND (a2.canvas_user_id=act.canvas_user_id))) AS "student total score",
+       sum(act.delta) over(partition by s.canvas_user_id order by act.updated_at) as "student total score",
        act.delta AS "action score", to_char(act.updated_at, 'YYYY-MM-DD HH24:MI:SS') AS "action at"
-    FROM activities act INNER JOIN students s ON act.canvas_user_id = s.canvas_user_id
-      WHERE (act.score = 't') AND (act.expired = 'f') AND (act.assigned_discussion = 'f');
+    FROM activities act
+    INNER JOIN students s ON act.canvas_user_id = s.canvas_user_id
+    WHERE (act.score = 't') AND (act.expired = 'f') AND (act.assigned_discussion = 'f')
+    ORDER BY act.updated_at ASC;
   END_OF_QUERY_SQL
 
 
